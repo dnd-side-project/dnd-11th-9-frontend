0675173538365ceafed868dd6c442300f029e02f
Zero1016 <iolpd42@gmail.com>
2024-09-06 14:05:56 +0900

    refactor: progress-bar를 분리합니다.

--- app/(beforeLogin)/onboarding.styles.tsx
+++ app/(beforeLogin)/onboarding.styles.tsx
@@ -7,44 +7,28 @@ const statusBarHeight = Constants.statusBarHeight || 0;
 
 export const Container = styled.SafeAreaView`
   flex: 1;
+  ${flexDirectionColumn};
+  justify-content: space-between;
+`;
+
+export const OnBoardingWrapper = styled.View`
+  ${flexDirectionColumn};
+  gap: 32px;
+  padding-horizontal: 32px;
 `;
 
 export const ContentBox = styled.View`
-  ${flexItemCenter};
-  flex-direction: column;
   justify-content: space-between;
-  padding-top: 135px;
   ${`padding-top: ${statusBarHeight + 135}px`};
 `;
 
 export const TextWrapper = styled.View`
   gap: 12px;
   width: 100%;
-  padding: 0 32px;
   ${flexDirectionColumn};
 `;
 
-export const ProgressBar = styled.View`
-  display: flex;
-  flex-direction: row;
-  gap: 6px;
-`;
-
-export const StepActive = styled.View`
-  width: 16px;
-  height: 6px;
-  background: #000;
-  border-radius: 30px;
-`;
-
-export const StepInactive = styled.View`
-  width: 6px;
-  height: 6px;
-  background-color: #00000026;
-  border-radius: 30px;
-`;
-
-export const StepBox = styled.View`
+export const ButtonBox = styled.View`
   ${flexDirectionColumn};
   gap: 12px;
   width: 100%;
---
+++ src/components/common/progress-bar/index.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/native';
+import { memo } from 'react';
+import type { ViewProps } from 'react-native';
+import Animated, { LinearTransition, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+
+type StepBarProps = {
+  isActive: boolean;
+};
+
+function StepBar({ isActive }: StepBarProps) {
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      marginVertical: 'auto',
+      marginHorizontal: 0,
+      width: 6,
+      height: withTiming(isActive ? 16 : 6, { duration: 500 }),
+      backgroundColor: withTiming(isActive ? '#000' : '#00000026', { duration: 500 }),
+      borderRadius: 30,
+      alignSelf: 'flex-start',
+    };
+  });
+
+  return (
+    <Animated.View
+      layout={LinearTransition.duration(300)}
+      style={animatedStyle}
+    />
+  );
+}
+
+type Props = {
+  currentStep: number;
+  stepLength: number;
+} & ViewProps;
+
+function ProgressBar({ currentStep, stepLength, ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      {Array.from({ length: stepLength }, (_, index) => {
+        return (
+          <StepBar
+            key={index}
+            isActive={index === currentStep}
+          />
+        );
+      })}
+    </Container>
+  );
+}
+
+export const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  gap: 6px;
+  height: 16px;
+`;
+
+export default memo(ProgressBar);
