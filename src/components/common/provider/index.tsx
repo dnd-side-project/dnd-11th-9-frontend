c89e279e1560539b10e06950bc1f30b3dd19a1ea
Zero1016 <iolpd42@gmail.com>
2024-09-15 14:03:08 +0900

    refactor: useAppOpen을 제거한다.

--- src/components/common/provider/index.tsx
+++ src/components/common/provider/index.tsx
@@ -2,17 +2,10 @@ import { ThemeProvider } from '@emotion/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import queryClient from '@/apis/queryClient';
-import { useAppOpen } from '@/hooks';
 import { theme } from '@/styles/theme';
 import type { PropsNeedChildren } from '@/types';
 
 const Provider = ({ children }: PropsNeedChildren) => {
-  const loaded = useAppOpen();
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,1 @@
-export * from './useAppOpen';
 export * from './useStorageState';
