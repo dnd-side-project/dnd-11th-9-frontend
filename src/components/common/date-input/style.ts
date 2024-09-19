0c0785bf9a4f8a0ca4e06a6269b5b78e99f1224f
Zero <iolpd42@gmail.com>
2024-09-20 01:42:28 +0900

    Feat/#41 Image Picker를 활용한 이미지 등록을 추가합니다. (#53)
    * feat: web용 모달 스타일을 추가한다.
    
    * feat: image-picker를 이용한 프로젝트 이미지 등록을 추가합니다.
    
    * feat: date-input을 작성합니다.

---
+++ src/components/common/date-input/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/native';
+
+import { flexItemCenter } from '@/styles/common';
+
+export const Container = styled.Pressable`
+  ${flexItemCenter};
+  padding: 18px 16px;
+  border-radius: 8px;
+`;
