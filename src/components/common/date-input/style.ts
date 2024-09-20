bbc34b77970be2c6eebbfbe2e87ea8d73a4726bc
Zero1016 <iolpd42@gmail.com>
2024-09-21 04:28:51 +0900

    feat: date-input을 스타일을 추가합니다.

--- src/components/common/date-input/style.ts
+++ src/components/common/date-input/style.ts
@@ -1,9 +1,11 @@
 import styled from '@emotion/native';
 
-import { flexItemCenter } from '@/styles/common';
+import { flexDirectionRowItemsCenter } from '@/styles/common';
 
 export const Container = styled.Pressable`
-  ${flexItemCenter};
+  ${flexDirectionRowItemsCenter};
+  flex-grow: 1;
   padding: 18px 16px;
+  background: ${({ theme }) => theme.color.Background.Normal};
   border-radius: 8px;
 `;
