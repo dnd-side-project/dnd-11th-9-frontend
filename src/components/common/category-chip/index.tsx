dca397c96e7efb5081b9eaa5a5587015f14520c3
Zero1016 <iolpd42@gmail.com>
2024-09-24 17:29:37 +0900

    refactor: 카테고리의 타입과 스타일을 분리합니다.

--- src/components/common/category-chip/index.tsx
+++ src/components/common/category-chip/index.tsx
@@ -1,47 +1,13 @@
-import type { ReactNativeStyle } from '@emotion/native';
-import { css } from '@emotion/native';
 import { memo } from 'react';
 
 import Typography from '@/components/common/typography';
-import { color } from '@/styles/theme';
+import { CategoryStyle } from '@/styles/category';
+import type { CategoryType } from '@/types/category';
 
 import * as S from './style';
 
-type Category = '기술' | '커뮤니케이션' | '성실성' | '협업' | '문서화' | '시간관리' | '리더십';
-
 type Props = {
-  category: Category;
-};
-
-const CategoryStyle: Record<Category, ReactNativeStyle> = {
-  기술: css`
-    background: ${color.Blue['95']};
-    border: 1px solid ${color.Blue['90']};
-  `,
-  성실성: css`
-    background: ${color.Pink['95']};
-    border: 1px solid ${color.Pink['90']};
-  `,
-  협업: css`
-    background: ${color.Orange['95']};
-    border: 1px solid ${color.Orange['90']};
-  `,
-  문서화: css`
-    background: ${color.Red['95']};
-    border: 1px solid ${color.Red['90']};
-  `,
-  커뮤니케이션: css`
-    background: ${color.LightBlue['95']};
-    border: 1px solid ${color.LightBlue['90']};
-  `,
-  시간관리: css`
-    background: ${color.Violet['95']};
-    border: 1px solid ${color.Violet['90']};
-  `,
-  리더십: css`
-    background: #e6fad9;
-    border: 1px solid #c3f0a3;
-  `,
+  category: CategoryType;
 };
 
 function CategoryChip({ category }: Props) {
